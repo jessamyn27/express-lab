9a5c17d1d330a4a7a63858ecc9e5dfec25ee41af
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-09 14:02:58 -0500

    up and running, greeting and name working

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 
 const app = express();
+const name = "jessamyn";
+
+app.get('/greeting/', (req, res) => {
+console.log("hello " + name) //params are the req
+  res.send("greetings " + req.params.name)
+});
+app.get('/greeting/:name', (req, res) => {
+  res.send("greetings " + req.params.name)
+});
 
 app.listen(3000, () => {
   console.log('port is running');
 });
+
+// app.get('/greeting/:name', (req, res) => {
+//   res.send({params: req.params, queries: req.query})
+// });
